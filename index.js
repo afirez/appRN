7fbdbeebf258c510da978009c5c1ab15be830e08
afirez <afirez@163.com>
2019-02-26 17:41:12 +0800

    feature react-navigation

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+import HomePage from './app/pages/home/HomePage';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => HomePage);
