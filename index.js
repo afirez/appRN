459942ebf98ce6d13e01e73e84f3c41bb2619a16
afirez <afirez@163.com>
2019-02-27 18:00:29 +0800

    feature ( BottomTabNavigator ) apply createBottomTabNavigator

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 import {AppRegistry} from 'react-native';
 // import App from './App';
 import HomePage from './app/pages/home/HomePage';
+import MainPage from './app/pages/home/MainPage';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => HomePage);
+AppRegistry.registerComponent(appName, () => MainPage);
